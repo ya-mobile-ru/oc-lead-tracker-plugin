2f8fbdf21dcada42055cb6e9d6003af20556e49b
evgenydmorozov <evgenydmorozov@gmail.com>
2023-03-09 11:17:32 +0500

    feat(controllers): add controller for leads

---
+++ controllers/Lead.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Yamobile\LeadTracker\Controllers;
+
+use Backend\Classes\Controller;
+use BackendMenu;
+
+class Lead extends Controller
+{
+    public $implement = [
+        'Backend\Behaviors\ListController',
+        'Backend\Behaviors\FormController',
+    ];
+
+    public $listConfig = 'config_list.yaml';
+
+    public $formConfig = 'config_form.yaml';
+
+    public function __construct()
+    {
+        parent::__construct();
+        BackendMenu::setContext('Yamobile.LeadTracker', 'LeadTracker', 'lead');
+    }
+}
