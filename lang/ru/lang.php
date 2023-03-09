9b97adfafdad04a5026dde1b57db95ddb746184d
evgenydmorozov <evgenydmorozov@gmail.com>
2023-03-09 10:36:15 +0500

    feat(lang): add Russian lang file

---
+++ lang/ru/lang.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'details' => [
+        'name' => 'Lead Tracker',
+        'description' => 'Система отслеживания лидов',
+        'author' => 'Ya-mobile',
+    ],
+    'lead' => [
+        'id' => 'ID',
+        'name' => 'Имя',
+        'name_placeholder' => 'Иван',
+        'phone' => 'Телефон',
+        'phone_placeholder' => '+7 (900) 000-00-00',
+        'email' => 'Email',
+        'email_placeholder' => 'admin@gmail.com',
+        'info' => 'Информация',
+        'info_placeholder' => 'Информация',
+        'source' => 'Источник',
+        'ip' => 'IP',
+        'user_agent' => 'User-Agent',
+        'created_at' => 'Создано',
+        'breadcrumb' => 'Лиды',
+    ],
+];
